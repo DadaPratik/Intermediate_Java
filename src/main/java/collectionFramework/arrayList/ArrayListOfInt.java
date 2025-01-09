5a99103cd5c7523cdf736a45a0f7a572d931ab53
DadaPratik <parajpra@sheridancollege.ca>
2025-01-09 20:19:35 +0545

    ArrayList

--- src/main/java/collectionFramework/arrayList/ArrayListOfInt.java
+++ src/main/java/collectionFramework/arrayList/ArrayListOfInt.java
@@ -7,6 +7,7 @@ public class ArrayListOfInt {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
+
         list.add(3);
 
         System.out.println(list);
