27a21490147471d13a356ee3a8d7a6eed1efb225
DadaPratik <parajpra@sheridancollege.ca>
2025-01-09 20:39:53 +0545

    Queue and Dequeue implementation by LinkedList

---
+++ src/main/java/queue/LinkedListAsQueue.java
@@ -0,0 +1,25 @@
+package queue;
+
+import java.util.LinkedList;
+
+public class LinkedListAsQueue {
+    // poll() - removes element from the front
+    // offer() - add element from the back
+
+    public static void main(String[] args) {
+
+        LinkedList<String> patient = new LinkedList<String>();
+        patient.add("1st patient");
+        patient.add("2nd patient");
+        patient.add("3rd patient");
+
+        System.out.println("original linkedList: " + patient);
+
+        patient.poll();
+        System.out.println("after poll: " + patient);
+
+        patient.offer("4th patient");
+        System.out.println("after offer: " + patient);
+
+    }
+}
